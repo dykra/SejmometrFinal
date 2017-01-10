3ee46347bef2cf0b1b27bea683f3d0e128494368
dykra <asiapalewicz@gmail.com>
2017-01-10 18:19:32 +0100

    argument's parser added

--- src/agh/cs/sejm/ArgumentsParser.java
+++ src/agh/cs/sejm/ArgumentsParser.java
@@ -1,43 +1,61 @@
 package agh.cs.sejm;
 
+import java.io.IOException;
+
 /**
  * Created by Joanna on 2016-12-15.
  */
 public class ArgumentsParser {
-//todo 7 jako aktualizacja plików
-    public int parseArguments(String[] args) {
-        int result=-1;
-        if (args.length <= 4 && args.length >=2 ) {
+    private JasonDownloader jasonDownloader;
+    private ExpensesAnaliser expensesAnaliser;
+    private TravelAnalyser travelAnalyser;
+
+
+
+    public void parseArguments(String[] args) throws IOException {
+        if (Integer.parseInt(args[0]) != 7 && Integer.parseInt(args[0]) !=8) throw new IllegalArgumentException(args[0] + "Podałeś nieprawidłowy numer kadencji.");
+            else {
+                jasonDownloader = new JasonDownloader();
+                jasonDownloader.initializePoliticians(Boolean.FALSE);
+                expensesAnaliser = new ExpensesAnaliser();
+                travelAnalyser = new TravelAnalyser();
+        }
+
+        if (args.length <= 4 && args.length >=2 || args.length == 3) {
             if (args.length == 4){
                 // gdy mamy imię i nazwisko
                 switch (args[3]){
                     case "all":
-                        result = 0;
+                        System.out.println(this.expensesAnaliser.politicianExpenses(args[1], args[2],Integer.parseInt(args[0]), jasonDownloader.politiciansSet).toString());
                         break;
-                    case "minorExpenses":
-                        result = 1;
+                    case "small":
+                        System.out.println(this.expensesAnaliser.pettyExpenses(args[1], args[2],Integer.parseInt(args[0]), jasonDownloader.politiciansSet).toString());
                         break;
                     default:
-                        throw new IllegalArgumentException(args[3] + "Dla konkrentego posła można podać tylko wszystkie lub drobne wydatki.");
+                        throw new IllegalArgumentException(args[3] + "   Dla konkrentego posła można podać tylko wszystkie lub drobne wydatki.");
                 }
             }
-            else {
+            else if(args.length == 2)  {
                 //nie mamy konkretnego posła tylko ogolne
                 switch (args[1]) {
                     case "avg":
-                        result = 2;
+                        System.out.println(this.expensesAnaliser.averageOfPoliticiansExpenses(jasonDownloader.politiciansSet,Integer.parseInt(args[0])).toString());
                         break;
                     case "traveller":
-                        result = 3;
+                        System.out.println(this.travelAnalyser.theBiggestAbroadTraveller(Integer.parseInt(args[0]), jasonDownloader.politiciansSet ));
                         break;
                     case "longestTimeAbroad":
-                        result = 4;
+                        System.out.println(this.travelAnalyser.theLongestAbroadTraveler(Integer.parseInt(args[0]), jasonDownloader.politiciansSet ));
                         break;
                     case "expensiveAbroad":
-                        result = 5;
+                        System.out.println(this.travelAnalyser.mostExpensiveTravel(Integer.parseInt(args[0]), jasonDownloader.politiciansSet ));
                         break;
                     case "italy":
-                        result = 6;
+                        System.out.println(this.travelAnalyser.italyTravelers(Integer.parseInt(args[0]), jasonDownloader.politiciansSet ));
+                        break;
+                    case "actualize":
+                        jasonDownloader.actualizePolitisians();
+                        System.out.println("Zaktualizowano pliki na dysku.");
                         break;
                     default:
                         throw new IllegalArgumentException(args[1] + "Podałeś nieprawidłowe argumenty.");
@@ -45,7 +63,5 @@ public class ArgumentsParser {
             }
         }else
             throw new IllegalArgumentException("Podano niewłasciwą ilość argumentów");
-
-        return result;}
-
+        }
 }
--- src/main/SejmSystem.java
+++ src/main/SejmSystem.java
@@ -1,12 +1,8 @@
 package main;
 
-import agh.cs.sejm.ExpensesAnaliser;
-import agh.cs.sejm.JasonDownloader;
-import agh.cs.sejm.Politicians;
-import agh.cs.sejm.TravelAnalyser;
+import agh.cs.sejm.*;
 
 import java.io.IOException;
-import java.util.LinkedList;
 
 /**
  * Created by Joanna on 2016-12-16.
@@ -15,39 +11,21 @@ public class SejmSystem {
     public static void main(String[] args) {
 
         try {
-            //ArgumentsParser parser = new ArgumentsParser();
-            //int result = parser.parseArguments(args);
-            //argumentu o nr kadencji i ewentualnego imienia i naziwka nie parsujemy -> prosto z args
-            //gdy imie i nazwisko potem sprawdzić, po wczytaniu czy istnieje i ewentualnie wyrzucić błąd
-            //result ma inf o tym ktora funkcje bd realizować
-
             final long startTime = System.currentTimeMillis();
 
-            JasonDownloader jasonDownloader = new JasonDownloader();
-            jasonDownloader.initializePoliticians(Boolean.FALSE);
-
-            ExpensesAnaliser a = new ExpensesAnaliser();
-
-            //System.out.print(a.politicianExpenses("Ewa", "Kopacz", 7, jasonDownloader.politiciansSet) + "\n");
-            //System.out.print(a.averageOfPoliticiansExpenses(jasonDownloader.politiciansSet,7).toString() + "\n" );
-            //System.out.print(a.pettyExpenses("Piotr", "Szeliga", 7, jasonDownloader.politiciansSet).toString()  + "\n");
+            ArgumentsParser parser = new ArgumentsParser();
+            parser.parseArguments(args);
 
             final long endTime = System.currentTimeMillis();
-            TravelAnalyser b = new TravelAnalyser();
-            LinkedList<Politicians> list = b.italyTravelers(8,jasonDownloader.politiciansSet);
-            //System.out.println(b.mostExpensiveTravel(7,jasonDownloader.politiciansSet));
-            System.out.println(b.theLongestAbroadTraveler(7,jasonDownloader.politiciansSet));
-            System.out.println(b.theBiggestAbroadTraveller(8,jasonDownloader.politiciansSet));
-            System.out.println(endTime-startTime);
+            System.out.println("Czas wykonania w sekundach:" + ((endTime-startTime)/1000));
+
         }catch (IllegalArgumentException ex){
             System.out.print(ex.getMessage());
 
-        } catch (IOException e) {
+        }catch (IOException e) {
             System.out.print("Wystąpił problem z wczytaniem pliku.   " + e.getMessage());
         }
 
-
     }
-
 
 }
